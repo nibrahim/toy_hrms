9f34d9758ee4fe74451ae5c76c09980691832d38
Noufal Ibrahim <noufal@nibrahim.net.in>
2023-11-22 11:21:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Introduction
+
+HR tool to manage employee information and leaves.
+
+# Basic usage
+
+## Operations
+- `hr.py --dbname=[hr] initdb` - Initialise the database.
+- `hr.py import {csvfile}` - Import entries from the CSV file
+- `hr.py generate` - Generates VCF files for all employees in database. Optional QR codes
---
+++ hr.py
@@ -0,0 +1,19 @@
+# import standard library (alphabetical order)
+# # free line
+# import 3rd party (alphabetical order)
+# # free line
+# import our own (alphabetical order)
+
+"""
+BEGIN:VCARD
+VERSION:2.1
+N:{lname};{fname}
+FN:{fname} {lname}
+ORG:Authors, Inc.
+TITLE:{title}
+TEL;WORK;VOICE:{phone}
+ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
+EMAIL;PREF;INTERNET:{email}
+REV:20150922T195243Z
+END:VCARD
+"""
