bdf4eb25b97b63a09bcec024c3a2c089cff0ad22
Noufal Ibrahim <noufal@nibrahim.net.in>
2023-11-30 11:28:42 +0530

    Adds designation table. Misc changes

--- data/init.sql
+++ data/init.sql
@@ -1,10 +1,16 @@
+CREATE TABLE designations (
+       id SERIAL PRIMARY KEY,
+       title VARCHAR(100),
+       leaves INTEGER
+       );
+
 CREATE TABLE employees(
        id SERIAL PRIMARY KEY,
        fname VARCHAR(50),
        lname VARCHAR(50),
-       designation VARCHAR(100),
        email VARCHAR(120),
-       phone VARCHAR(50)
+       phone VARCHAR(50),
+       rank INTEGER REFERENCES designations(id)
        );
 
 CREATE TABLE leaves (
@@ -14,3 +20,4 @@ CREATE TABLE leaves (
        reason VARCHAR(200),
        UNIQUE (employee, date) -- An employee can take only one leave on a given day
        );
+
--- hr.py
+++ hr.py
@@ -11,19 +11,18 @@ class HRException(Exception): pass
 
 logger = False
 
-"""
-BEGIN:VCARD
+def generate_vcard(lname, fname, designation, email, phone):
+    return f"""BEGIN:VCARD
 VERSION:2.1
 N:{lname};{fname}
 FN:{fname} {lname}
 ORG:Authors, Inc.
-TITLE:{title}
+TITLE:{designation}
 TEL;WORK;VOICE:{phone}
 ADR;WORK:;;100 Flat Grape Dr.;Fresno;CA;95555;United States of America
 EMAIL;PREF;INTERNET:{email}
 REV:20150922T195243Z
-END:VCARD
-"""
+END:VCARD"""
 
 
 def init_logger(is_verbose):
@@ -49,8 +48,13 @@ def parse_args():
     import_parser = subparsers.add_parser("import", help="Import data from csv file")
     import_parser.add_argument("employees_file", help="List of employees to import")
 
+    query_parser = subparsers.add_parser("query", help="Get information for a single employee")
+    query_parser.add_argument("--vcard", action="store_true", default=False, help="Generate vcard for employee")
+    query_parser.add_argument("id", help="employee id")
+
     args = parser.parse_args()
     return args
+
 
 def handle_initdb(args):
     with open("data/init.sql") as f:
@@ -75,7 +79,27 @@ def handle_import(args):
             cur.execute(query, (lname, fname, designation, email, phone))
         con.commit()
     
+def handle_query(args):
+    con = psycopg2.connect(dbname=args.dbname)
+    cur = con.cursor()
+    query = "SELECT fname, lname, designation, email, phone from employees where id = %s"
+    cur.execute(query, (args.id,))
+    fname, lname, designation, email, phone = cur.fetchone()
 
+    print (f"""Name        : {fname} {lname}
+Designation : {designation}
+Email       : {email}
+Phone       : {phone}""")
+    if (args.vcard):
+        vcard = generate_vcard(lname, fname, designation, email, phone)
+        print (f"\n{vcard}")
+
+
+    con.close()
+            
+
+
+    
 
 
 def main():
@@ -83,7 +107,8 @@ def main():
         args = parse_args()
         init_logger(args.v)
         ops = {"initdb" : handle_initdb,
-               "import" : handle_import}
+               "import" : handle_import,
+               "query" : handle_query}
         ops[args.op](args)
     except HRException as e:
         logger.error("Program aborted, %s", e)
